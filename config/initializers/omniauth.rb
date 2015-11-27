c4879f5d094a0c2cd7279c29545b9fa510748ac7
dguardio <akinwoleolaopeyemi@gmail.com>
2015-11-27 21:45:23 +0000

    Create a Sessions Controller to handle creating sessions and session failures

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+	provider :twitter, ENV['cTuR8IaBWpab43n6uD6hb3DV1'], ENV['qZTHqECT5I0D1U9xLaJkKB1ycRw1OK300638Wjd9H2HKGC845x']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/failure'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   root 'page#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
