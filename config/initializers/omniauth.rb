51c097c04a3f901cc64af016998e013947231bfa
dguardio <akinwoleolaopeyemi@gmail.com>
2015-11-27 22:34:18 +0000

    Modify sessions controller and routes to prevent 400 error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :twitter, ENV['cTuR8IaBWpab43n6uD6hb3DV1'], ENV['qZTHqECT5I0D1U9xLaJkKB1ycRw1OK300638Wjd9H2HKGC845x']
+	provider :twitter, 'cTuR8IaBWpab43n6uD6hb3DV1', 'qZTHqECT5I0D1U9xLaJkKB1ycRw1OK300638Wjd9H2HKGC845x'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/failure'
-
-  # The priority is based upon order of creation: first created -> highest priority.
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   root 'page#index'
